df1e001249d91b6fed529864353eef5b4d8a7032
lmparedes1977 <lmparedes@gmail.com>
2025-03-23 14:49:31 -0300

    updates

---
+++ src/main/java/com/cardboard/service/BoardService.java
@@ -0,0 +1,31 @@
+package com.cardboard.service;
+
+import com.cardboard.dao.BoardDao;
+import lombok.AllArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@AllArgsConstructor
+public class BoardService {
+
+    private final Connection connection;
+
+    public boolean delete(final Long id) throws SQLException {
+        var dao = new BoardDao(connection);
+        try {
+            if(!dao.exists(id)){
+                return false;
+            }
+            dao.delete(id);
+            connection.commit();
+            return true;
+        } catch (SQLException e) {
+            connection.rollback();
+            throw e;
+        }
+    }
+
+}
+
+
